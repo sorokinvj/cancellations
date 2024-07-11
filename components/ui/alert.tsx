f996263bd7172033ed69c5ddd8126046d979887e
Vladislav Sorokin <sorokinvj@gmail.com>
2024-07-11 17:47:19 +0100

    chore: clean up

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -6,11 +6,11 @@ import {
   Transition as HeadlessTransition,
   TransitionChild as HeadlessTransitionChild,
   type DialogProps as HeadlessDialogProps,
-} from '@headlessui/react'
-import clsx from 'clsx'
-import type React from 'react'
-import { Fragment } from 'react'
-import { Text } from './text'
+} from '@headlessui/react';
+import clsx from 'clsx';
+import type React from 'react';
+import { Fragment } from 'react';
+import { Text } from './text';
 
 const sizes = {
   xs: 'sm:max-w-xs',
@@ -22,7 +22,7 @@ const sizes = {
   '3xl': 'sm:max-w-3xl',
   '4xl': 'sm:max-w-4xl',
   '5xl': 'sm:max-w-5xl',
-}
+};
 
 export function Alert({
   open,
@@ -31,7 +31,10 @@ export function Alert({
   className,
   children,
   ...props
-}: { size?: keyof typeof sizes; children: React.ReactNode } & HeadlessDialogProps) {
+}: {
+  size?: keyof typeof sizes;
+  children: React.ReactNode;
+} & HeadlessDialogProps) {
   return (
     <HeadlessTransition appear as={Fragment} show={open} {...props}>
       <HeadlessDialog onClose={onClose}>
@@ -42,18 +45,19 @@ export function Alert({
           enterTo="opacity-100"
           leave="ease-in duration-100"
           leaveFrom="opacity-100"
-          leaveTo="opacity-0">
+          leaveTo="opacity-0"
+        >
           <div className="bg-zinc-950/15 fixed inset-0 flex w-screen justify-center overflow-y-auto p-2 focus:outline-0 dark:bg-zinc-950/50 sm:px-6 sm:py-8 lg:px-8 lg:py-16" />
         </HeadlessTransitionChild>
 
         <HeadlessTransitionChild
-          className="fixed inset-0 w-screen overflow-y-auto pt-6 sm:pt-0"
           enter="ease-out duration-100"
           enterFrom="opacity-0"
           enterTo="opacity-100"
           leave="ease-in duration-100"
           leaveFrom="opacity-100"
-          leaveTo="opacity-0">
+          leaveTo="opacity-0"
+        >
           <div className="grid min-h-full grid-rows-[1fr_auto_1fr] justify-items-center p-8 sm:grid-rows-[1fr_auto_3fr] sm:p-4">
             <HeadlessTransitionChild
               as={HeadlessDialogPanel}
@@ -67,17 +71,21 @@ export function Alert({
               enterTo="scale-100"
               leave="ease-in duration-100"
               leaveFrom="scale-100"
-              leaveTo="scale-100">
+              leaveTo="scale-100"
+            >
               {children}
             </HeadlessTransitionChild>
           </div>
         </HeadlessTransitionChild>
       </HeadlessDialog>
     </HeadlessTransition>
-  )
+  );
 }
 
-export function AlertTitle({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
+export function AlertTitle({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
   return (
     <HeadlessDialogTitle
       {...props}
@@ -86,24 +94,33 @@ export function AlertTitle({ className, ...props }: React.ComponentPropsWithoutR
         'text-balance sm:text-wrap text-center text-base/6 font-semibold text-zinc-950 dark:text-white sm:text-left sm:text-sm/6',
       )}
     />
-  )
+  );
 }
 
-export function AlertDescription({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
+export function AlertDescription({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
   return (
     <HeadlessDescription
       as={Text}
       {...props}
       className={clsx(className, 'text-pretty mt-2 text-center sm:text-left')}
     />
-  )
+  );
 }
 
-export function AlertBody({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
-  return <div {...props} className={clsx(className, 'mt-4')} />
+export function AlertBody({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
+  return <div {...props} className={clsx(className, 'mt-4')} />;
 }
 
-export function AlertActions({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
+export function AlertActions({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
   return (
     <div
       {...props}
@@ -112,5 +129,5 @@ export function AlertActions({ className, ...props }: React.ComponentPropsWithou
         '*:w-full sm:*:w-auto mt-6 flex flex-col-reverse items-center justify-end gap-3 sm:mt-4 sm:flex-row',
       )}
     />
-  )
+  );
 }
