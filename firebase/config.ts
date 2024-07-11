0263339ab43934e53babb32a34c9e3b8b3d2c3de
Vladislav Sorokin <sorokinvj@gmail.com>
2024-07-11 15:46:32 +0100

    chore: remove keys

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBORZsvynFAQSl0Nt5-sj8imFUPt48_9vk",
-  authDomain: "meet-assistant-6d8ad.firebaseapp.com",
-  projectId: "meet-assistant-6d8ad",
-  storageBucket: "meet-assistant-6d8ad.appspot.com",
-  messagingSenderId: "255174193709",
-  appId: "1:255174193709:web:1ae9edb4f3405d365afea4",
-  measurementId: "G-3VQS4Z19WZ"
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 const app = initializeApp(firebaseConfig);
