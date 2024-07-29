ac3f9eb8ef36eae4d93099fff7f4ec4aa9f2433d
Vladislav Sorokin <sorokinvj@gmail.com>
2024-07-29 15:01:47 +0100

    fix: user info card

--- components/RequestsTable/UserInfoCard.tsx
+++ components/RequestsTable/UserInfoCard.tsx
@@ -5,26 +5,26 @@ interface UserInfoCardProps {
   request: Request;
 }
 
-const UserInfoCard: FC<UserInfoCardProps> = ({ request }) => {
+const UserInfoCard: FC<UserInfoCardProps> = ({ request: { customerInfo } }) => {
   return (
     <div className="bg-white shadow-md rounded-lg p-6 max-w-md mx-auto">
       <h2 className="text-2xl font-semibold mb-4">User Information</h2>
       <div className="space-y-3">
         <p>
           <span className="font-medium">Customer Name:</span>{' '}
-          {request.customerName}
+          {customerInfo.customerName}
         </p>
         <p>
           <span className="font-medium">Customer Email:</span>{' '}
-          {request.customerEmail}
+          {customerInfo.customerEmail}
         </p>
         <p>
           <span className="font-medium">Account Number:</span>{' '}
-          {request.accountNumber}
+          {customerInfo.accountNumber}
         </p>
         <p>
           <span className="font-medium">Last Four CC Digits:</span>{' '}
-          {request.lastFourCCDigits}
+          {customerInfo.lastFourCCDigits}
         </p>
       </div>
     </div>
