56a1529d8f16df5697c0ff8749a27762de9b6569
Vladislav Sorokin <sorokinvj@gmail.com>
2024-08-09 18:26:20 +0100

    fix: status

--- components/RequestStatus/RequestStatus.tsx
+++ components/RequestStatus/RequestStatus.tsx
@@ -5,7 +5,7 @@ const RequestStatus = ({ status }: { status: RequestStatusType }) => {
     Pending: 'bg-sky-100 text-sky-800',
     Canceled: 'bg-green-100 text-green-800',
     Declined: 'bg-red-100 text-red-800',
-    'Save Offered': 'bg-amber-100 text-amber-200',
+    'Save Offered': 'bg-amber-100 text-amber-600',
     'Save Declined': 'bg-amber-100 text-amber-400',
     'Save Accepted': 'bg-amber-100 text-amber-500',
     'Save Confirmed': 'bg-amber-100 text-amber-800',
